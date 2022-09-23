77fd4ef76df962225b9223fc5eb46e4df940bda0
rodrigomonice <71037260+rodrigomonice@users.noreply.github.com>
2022-09-23 01:23:21 -0300

    Ajustadas as cores no teclado conforme foi solicitado

--- App.js
+++ App.js
@@ -77,12 +77,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1e1240'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': '#79737e'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -98,17 +98,17 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#1e1240"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
     textAlign: "right"
   },
   historyText:{
-    color: "#7c7c7c",
+    color: "gray",
     fontSize: 20,
     marginRight: 10,
     alignSelf: 'flex-end',
@@ -118,7 +118,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3d0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
     flex: 2,
   },
   textButton: {
-    color: "#7c7c7c",
+    color: "#ffffff",
     fontSize: 20,
   } 
 });
